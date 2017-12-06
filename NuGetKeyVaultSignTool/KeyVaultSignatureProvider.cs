b8286efc754a3460e809aac36c6867b245aedeaa
Oren Novotny <oren@novotny.org>
2017-12-06 11:23:29 -0500

    Support SubjectKeyInfo if available

--- NuGetKeyVaultSignTool/KeyVaultSignatureProvider.cs
+++ NuGetKeyVaultSignTool/KeyVaultSignatureProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
@@ -10,12 +11,12 @@ using NuGet.Common;
 using NuGet.Packaging.Signing;
 using NuGetKeyVaultSignTool.BouncyCastle;
 using Org.BouncyCastle.Asn1;
-using Org.BouncyCastle.Asn1.Cms;
 using Org.BouncyCastle.Asn1.Esf;
 using Org.BouncyCastle.Cms;
 using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509.Extension;
 using Org.BouncyCastle.X509.Store;
-using X509Certificate = Org.BouncyCastle.X509.X509Certificate;
+using AttributeTable = Org.BouncyCastle.Asn1.Cms.AttributeTable;
 
 namespace NuGetKeyVaultSignTool
 {
@@ -47,68 +48,80 @@ namespace NuGetKeyVaultSignTool
                 throw new ArgumentNullException(nameof(logger));
             }
 
-            var authorSignature = CreateKeyVaultSignature(request.Certificate, signatureContent, request.SignatureType);
+            var authorSignature = CreateKeyVaultSignature(request, signatureContent, request.SignatureType);
             var timestamped = await TimestampSignature(request, logger, authorSignature, token);
 
             return timestamped;
         }
 
-        byte[] CreateKeyVaultSignature(X509Certificate2 publicCert, SignatureContent signatureContent, SignatureType signatureType)
+        byte[] CreateKeyVaultSignature(SignPackageRequest request, SignatureContent signatureContent, SignatureType signatureType)
         {
-            List<X509Certificate> additionals = new List<X509Certificate>();
-            using (var chain = new X509Chain())
-            {
-                SetCertBuildChainPolicy(chain, DateTime.Now);
-                if (chain.Build(publicCert))
-                {
-                    // Get the chain as bc certs
-                    additionals.AddRange(chain.ChainElements.Cast<X509ChainElement>()
-                                              .Select(ce => DotNetUtilities.FromX509Certificate(ce.Certificate)));
-                }
-                else
-                {
-                    foreach (var chainStatus in chain.ChainStatus)
-                    {
-                        if (chainStatus.Status != X509ChainStatusFlags.NoError)
-                        {
-                            throw new NuGet.Packaging.Signing.SignatureException($"Certificate chain validation failed with error: {chainStatus.Status}");
-                        }
-                    }
-                }
-
-            }
-            
-
-            var bcCer = DotNetUtilities.FromX509Certificate(publicCert);
-
+            // Get the chain
+            var certs = SigningUtility.GetCertificateChain(request.Certificate, new X509Certificate2Collection());
+                
+            // Get the chain as bc certs
+            var additionals = certs.Select(DotNetUtilities.FromX509Certificate).ToList();
+            var bcCer = DotNetUtilities.FromX509Certificate(request.Certificate);
             var store = X509StoreFactory.Create("Certificate/Collection", new X509CollectionStoreParameters(additionals));
 
+
+            
+
+            // Attributes
             var cti = new CommitmentTypeIndication(new DerObjectIdentifier(AttributeUtility.GetSignatureTypeOid(signatureType)));
 
             // CommitmentTypeIdentifier attribute
             var attr = new Org.BouncyCastle.Asn1.Cms.Attribute(new DerObjectIdentifier(Oids.CommitmentTypeIndication), new DerSet(cti));
+            var attribTable = new AttributeTable(new Asn1EncodableVector { attr });
 
-            var asnvect = new Asn1EncodableVector
-            {
-                attr
-            };
-            var attribTable = new AttributeTable(asnvect);
-
-
-            var generator = new CmsSignedDataGenerator();
-            var builder = new SignerInfoGeneratorBuilder();
-            builder.WithSignedAttributeGenerator(new DefaultSignedAttributeTableGenerator(attribTable));
-            var b = builder.Build(new RsaSignatureFactory("SHA256WITHRSA", provider), bcCer);
-            generator.AddSignerInfoGenerator(b);
-            generator.AddCertificates(store);
+            // SignerInfo generator setup
+            var signerInfoGeneratorBuilder = new SignerInfoGeneratorBuilder()
+                .WithSignedAttributeGenerator(new DefaultSignedAttributeTableGenerator(attribTable));
             
 
+            // Subject Key Identifier (SKI) is smaller and less prone to accidental matching than issuer and serial
+            // number.  However, to ensure cross-platform verification, SKI should only be used if the certificate
+            // has the SKI extension attribute.
+
+            // Try to look for the value 
+            
+            var ext = bcCer.GetExtensionValue(new DerObjectIdentifier(Oids.SubjectKeyIdentifier));
+            SignerInfoGenerator signerInfoGenerator;
+            if (ext != null)
+            {
+                var ski = new SubjectKeyIdentifierStructure(ext);
+                signerInfoGenerator = signerInfoGeneratorBuilder.Build(new RsaSignatureFactory(HashAlgorithmToBouncyCastle(request.SignatureHashAlgorithm), provider), ski.GetKeyIdentifier());
+            }
+            else
+                signerInfoGenerator = signerInfoGeneratorBuilder.Build(new RsaSignatureFactory(HashAlgorithmToBouncyCastle(request.SignatureHashAlgorithm), provider), bcCer);
+
+            
+            var generator = new CmsSignedDataGenerator();
+            
+            generator.AddSignerInfoGenerator(signerInfoGenerator);
+            generator.AddCertificates(store);
+            
             var msg = new CmsProcessableByteArray(signatureContent.GetBytes());
             var data = generator.Generate(msg, true);
 
             var encoded = data.GetEncoded();
-
             return encoded;
+        }
+
+        static string HashAlgorithmToBouncyCastle(NuGet.Common.HashAlgorithmName algorithmName)
+        {
+            switch (algorithmName)
+            {
+                case NuGet.Common.HashAlgorithmName.SHA256:
+                    return "SHA256WITHRSA";
+                case NuGet.Common.HashAlgorithmName.SHA384:
+                    return "SHA384WITHRSA";
+                case NuGet.Common.HashAlgorithmName.SHA512:
+                    return "SHA512WITHRSA";
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(algorithmName));
+            }
         }
 
 
@@ -125,20 +138,6 @@ namespace NuGetKeyVaultSignTool
             return timestampProvider.TimestampSignatureAsync(timestampRequest, logger, token);
         }
 
-        static void SetCertBuildChainPolicy(
-            X509Chain x509Chain,
-            DateTime verificationTime)
-        {
-            var policy = x509Chain.ChainPolicy;
-
-          
-            policy.ApplicationPolicy.Add(new Oid(Oids.CodeSigningEkuOid));
-            
-            policy.RevocationFlag = X509RevocationFlag.ExcludeRoot;
-            policy.RevocationMode = X509RevocationMode.Online;
-
-            policy.VerificationTime = verificationTime;
-        }
 
     }
 }
